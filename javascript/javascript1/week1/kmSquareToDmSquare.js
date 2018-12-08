20c5140d9295ffd6b17912d04a0f9c410ccd00ff
Sana <sanahayyat116@live.com>
2018-12-08 01:13:37 +0100

    Commiting km^2 to dm^2 conversion file.

---
+++ javascript/javascript1/week1/kmSquareToDmSquare.js
@@ -0,0 +1,6 @@
+//Converting X km^2 into X dm^2
+
+let xKmSquare = 5;
+let xDmSquare = xKmSquare * 100000000;
+console.log("In Decimal Notation: " + xKmSquare + " km^2 = " + xDmSquare + " dm^2");
+console.log("In Scientific Notation: " + xKmSquare + " km^2 = " + xKmSquare + "e" + "+8 dm^2");
