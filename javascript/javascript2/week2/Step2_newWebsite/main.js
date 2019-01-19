079890c665ac4da664cde8607878f56d9f2d200e
Sana <sanahayyat116@live.com>
2019-01-19 14:24:39 +0100

    removing repetation in code

--- javascript/javascript2/week2/Step2_newWebsite/main.js
+++ javascript/javascript2/week2/Step2_newWebsite/main.js
@@ -3,6 +3,7 @@ let arrayOfDividentsOfThree = [];
 let arrayOfDividentsOfFive = [];
 let arrayOfDividentsOfThreeAndFive = [];
 
+let resultField =  document.querySelector('.resultField');
 //adding event listener to the button.
 document.querySelector('.dividentChecking').addEventListener('click', () => {
 
@@ -10,14 +11,14 @@ document.querySelector('.dividentChecking').addEventListener('click', () => {
     const inputValue = document.querySelector('input').value;
     if (inputValue) {
         if (isNaN(inputValue)) {
-            document.querySelector('.resultField').innerHTML = 'Please insert only number value!';
+            resultField.innerHTML = 'Please insert only number value!';
             alert("Must input numbers");
         } else {
-            document.querySelector('.resultField').innerHTML = inputValue;
+           resultField.innerHTML = inputValue;
             threeFive(inputValue, threeCallBlue, fiveCallYellow, threeAndFiveCallRed); //Using call back functions to change color of result d´field according to number!
         }
     } else {
-        document.querySelector('.resultField').innerHTML = 'Please insert value in text field!';
+        resultField.innerHTML = 'Please insert value in text field!';
     }
 
 })
@@ -27,27 +28,30 @@ function threeFive(startValue, endValue, threeCallBack, fiveCallBack) {
     for (let i = startValue; i <= endValue; i++) {
         calculatedArray.push(i);
         if (i % 3 == 0 && i % 5 == 0) {
+            resultField.innerHTML = startValue + "<br> is a divident of both 3 and 5.";
             threeAndFiveCallRed();      //if input number is divided by both 3 and 5 set background-color red
         } else if (i % 3 == 0) {
+            resultField.innerHTML = startValue + "<br> is a divident of 3.";
             threeCallBlue();            //if input number is divided by 3 set background-color blue
         } else if (i % 5 == 0) {
+            resultField.innerHTML = startValue + "<br> is a divident of 5.";
             fiveCallYellow();           //if input number is divided by 5 set background-color yellow
         } else {
-            document.querySelector('.resultField').style.backgroundColor = 'transparent';
-            document.querySelector('.resultField').innerHTML = '<br> Input is not divident of either 3 or 5!';
+            resultField.style.backgroundColor = 'transparent';
+            resultField.innerHTML = startValue + '<br> is not divident of either 3 or 5!';
         }
     }
 };
 //making background blue
 function threeCallBlue() {
-    document.querySelector('.resultField').style.backgroundColor = 'blue';
+    resultField.style.backgroundColor = 'blue';
 };
 //making background blue
 function fiveCallYellow() {
-    document.querySelector('.resultField').style.backgroundColor = 'yellow';
+    resultField.style.backgroundColor = 'yellow';
 };
 //making background red
 function threeAndFiveCallRed() {
-    document.querySelector('.resultField').style.backgroundColor = 'red';
+    resultField.style.backgroundColor = 'red';
 }
 
