e78bf65b8639e6ce6a862ab3a287bdd76171828e
Sana <sanahayyat116@live.com>
2019-01-19 22:47:58 +0100

    adding a const and a normal function and calling them.

--- javascript/javascript2/week2/Step2.5_functions/main.js
+++ javascript/javascript2/week2/Step2.5_functions/main.js
@@ -3,7 +3,18 @@ const arrayOfFunctions = [() => console.log('function # 1'),
                           () => console.log('function # 2'),
                           () => console.log('function # 3')];
 
-//Calling all function of arrayOfFunctions
-arrayOfFunctions.forEach(function (func) {
+arrayOfFunctions.forEach(function (func) {                    //Calling all function of arrayOfFunctions
     func();
 });
+
+const constFunction = () => {                                 // A const function.
+        console.log('This is a const function.');
+}
+
+function normalFunction(){                                   //Creating function normal way.
+    console.log('This function is created normal way.');
+}
+
+//Calling both functions.
+constFunction();
+normalFunction();
